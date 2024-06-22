16576f349e65767bc897b111d8106cb186bd726b
Ollie <ollie.nicholson@gmail.com>
2024-06-22 20:59:46 +1000

    Init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UrbanHire</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php include 'header.php';?>
+    <h1>
+        Welcome to UrbanHire
+    </h1>
+    <h2>
+        Rent Premium Equipment or Hire Local Tradesmen
+    </h2>
+    <?php include 'footer.php';?>
+    </body>
+</html>

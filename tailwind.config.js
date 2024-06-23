a2aa6eb19bff121c502fa484f350f54dd497be53
Ollie <ollie.nicholson@gmail.com>
2024-06-23 15:24:11 +1000

    testing deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     // https://tailwindcss.com/docs/content-configuration for more info
-    './index.php'
+    './api/**/*.php', // Adjust this to include all PHP files in your project
+    './*.php', // If you have any PHP files in the root directory
   ],
   theme: {
     extend: {},
